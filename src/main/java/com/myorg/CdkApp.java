69bac6895bc770c54ed3b9b39019e1bb94d2a9af
Elkin Torres <elktorre@WKMZT2B0C47C.global.publicisgroupe.net>
2023-11-06 22:26:20 -0500

    Add Maven Wrapper and SpringBootApplicationStack

--- src/main/java/com/myorg/CdkApp.java
+++ src/main/java/com/myorg/CdkApp.java
@@ -1,16 +1,20 @@
 package com.myorg;
 
+import dev.stratospheric.cdk.SpringBootApplicationStack;
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
-import software.amazon.awscdk.StackProps;
+//import software.amazon.awscdk.Environment;
+//import software.amazon.awscdk.StackProps;
 
-import java.util.Arrays;
+//import java.util.Arrays;
+
+import static java.util.Objects.requireNonNull;
 
 public class CdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new CdkStack(app, "CdkStack", StackProps.builder()
+//        new CdkStack(app, "CdkStack", StackProps.builder()
                 // If you don't specify 'env', this stack will be environment-agnostic.
                 // Account/Region-dependent features and context lookups will not work,
                 // but a single synthesized template can be deployed anywhere.
@@ -33,10 +37,25 @@ public class CdkApp {
                         .build())
                 */
 
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-                .build());
+        // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+//        .build());
+
+        String accountId = (String) app.getNode().tryGetContext("accountId");
+        requireNonNull(accountId, "context variable 'accountId' must not be null");
+
+        String region = (String) app.getNode().tryGetContext("region");
+        requireNonNull(region, "context variable 'region' must not be null");
+
+        new SpringBootApplicationStack(app, "SpringBootApplication", makeEnv(accountId, region),
+                "docker.io/stratospheric/todo-app-v1:latest");
 
         app.synth();
     }
-}
 
+    private static Environment makeEnv(String accountId, String region) {
+        return Environment.builder()
+                .account(accountId)
+                .region(region)
+                .build();
+    }
+}
